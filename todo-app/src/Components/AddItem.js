cac89173f0d76598ea727dab56aacab5188117c7
Marcos Acosta <macosta1625@gmail.com>
2021-10-28 00:23:09 -0700

    Revert "Merge lab2 into main"
    This reverts commit f246ae9940ae2fe788b1310fae3dc51126ef5f6b, reversing
    changes made to dd0be2708b93b595978a370bb7a8e8ca81b0e6b8.

--- todo-app/src/Components/AddItem.js
+++
@@ -1,9 +0,0 @@
-import './AddItem.css'
-
-export default function AddItem(props) {
-  return (
-    <button id="addItemFooter" onClick={() => props.addTaskAndEdit()} disabled={props.inEditMode}>
-      + add item
-    </button>
-  )
-}
