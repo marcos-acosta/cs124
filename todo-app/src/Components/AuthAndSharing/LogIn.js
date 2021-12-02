fd65ffd81f9a6d55dca672d25935d2a729cf9458
Marcos Acosta <macosta1625@gmail.com>
2021-12-02 01:04:12 -0800

    Partially style sign in page

--- todo-app/src/Components/AuthAndSharing/LogIn.js
+++ todo-app/src/Components/AuthAndSharing/LogIn.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat";
 import { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import "./LogIn.css";
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
@@ -11,15 +12,23 @@ export default function LogIn(props) {
 
   return (
     <>
-      <h1>Log in</h1>
-      <button onClick={() => props.auth.signInWithPopup(googleProvider)}>Sign in with Google</button>
+      <div className="authTitle">
+        log in
+      </div>
       <hr />
-      {error && `${error}`}
-      Email: <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
-      <br />
-      Password: <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
-      <br />
-      <button onClick={() => signInWithEmailAndPassword(email, password)}>Sign in</button>
+      <form>
+        <input value={email} onChange={e => setEmail(e.target.value)} placeholder="email" type="email" className="signInInput"/>
+        <input value={password} onChange={e => setPassword(e.target.value)} placeholder="password" type="password" className="signInInput" />
+        <button onClick={() => signInWithEmailAndPassword(email, password)} className="signInButton">sign in</button>
+      </form>
+      <hr />
+      <div className="centerContainer">
+        <button onClick={() => props.auth.signInWithPopup(googleProvider)} className="useGoogleButton">
+          <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png"
+                className="googleLogo"/>
+          sign in with google
+        </button>
+      </div>
     </>
   )
 }
--- todo-app/src/Components/AuthAndSharing/UnauthenticatedPage.js
+++ todo-app/src/Components/AuthAndSharing/UnauthenticatedPage.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
+import "./UnauthenticatedPage.css";
 
 export default function UnauthenticatedPage(props) {
+  const [showLogIn, setShowLogIn] = useState(true);
   return (
     <>
-      <LogIn auth={props.auth} />
-      <SignUp auth={props.auth}/>
+      <div className="header">
+        <h4>
+          <span className="todo">todo</span> 🦑
+        </h4>
+      </div>
+      <div className="centerContainer">
+        <div className="authenticationContainer">
+          {
+            showLogIn
+              ? <LogIn auth={props.auth} showSignUp={() => setShowLogIn(false)} />
+              : <SignUp auth={props.auth} showLogIn={() => setShowLogIn(true)}/>
+          }
+        </div>
+      </div>
     </>
   )
 }
