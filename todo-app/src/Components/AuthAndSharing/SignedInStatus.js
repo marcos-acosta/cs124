bfa83425272dbc42243648ab3011e84ed0e682ec
Marcos Acosta <macosta1625@gmail.com>
2021-12-06 20:12:32 -0800

    Make account options look pretty

--- todo-app/src/Components/AuthAndSharing/SignedInStatus.js
+++ todo-app/src/Components/AuthAndSharing/SignedInStatus.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import AccountIcon from "./../../images/account_icon_3.png"
+import "./SignedInStatus.css";
+
 export default function SignedInStatus(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div>
-      {props.user.email}
-      <button onClick={() => props.auth.signOut()}>Sign out</button>
+      <div className="accountIconDiv" onClick={() => setIsExpanded(!isExpanded)}>
+        <img src={AccountIcon} alt="account icon" className="accountIcon" />
+        <div className={`expandCaretDiv ${isExpanded ? "flipped" : ""}`}>⌵</div>
+      </div>
       {
-        !props.user.emailVerified && <button onClick={() => props.user.sendEmailVerification()}>Validate email</button>
+        isExpanded && <div className="accountPanel">
+          <div className="userEmail">
+            {props.user.email}
+          </div>
+          {
+            !props.user.emailVerified && <div>
+              <button onClick={() => props.user.sendEmailVerification()} className="accountButton validateButton">confirm email</button>
+            </div>
+          }
+          <div>
+            <button onClick={() => props.auth.signOut()} className="accountButton signOutButton">sign out</button>
+          </div>
+        </div>
+      }
+      {
+        !props.user.emailVerified && <div className="notification">_</div>
       }
     </div>
   )
